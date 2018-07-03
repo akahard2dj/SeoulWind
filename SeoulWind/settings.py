1c5a738d43653ad38d8566e169dc3a6a7e965f06
akahard2dj <akahard2dj@naver.com>
2018-07-03 09:33:19 +0900

    Update settings.py

--- SeoulWind/settings.py
+++ SeoulWind/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'windows.apps.WindowsConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,5 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/css"),
+    os.path.join(BASE_DIR, "static/data"),
+    os.path.join(BASE_DIR, "static/js"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
