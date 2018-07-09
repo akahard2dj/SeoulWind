765999bf0b6305bcc90366df656d26f50388ccc5
akahard2dj <akahard2dj@naver.com>
2018-07-09 17:23:47 +0900

    Update urls.py

--- windows/urls.py
+++ windows/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from windows import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('data/current/', views.get_data),
+    path('map/current/<data_type>', views.redirect_page),
 ]
