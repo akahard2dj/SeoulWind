34ab0c5a5e83c48ea0c337a63be8c3939fdc9bf1
akahard2dj <akahard2dj@naver.com>
2018-07-12 16:43:05 +0900

    Update test_air_config.js

--- windows/static/windows/test_air_config.js
+++ windows/static/windows/test_air_config.js
@@ -2,7 +2,7 @@
 requirejs.config({
     paths: {
         'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min',
-        'd3': 'https://storage.googleapis.com/seoulwind/static/d3.v3.min',
+        'd3': 'https://storage.googleapis.com/seoulwind/static/js/d3.v3.min',
         'topojson': 'https://storage.googleapis.com/seoulwind/static/windows/js/topojson.v1',
         'when': 'https://storage.googleapis.com/seoulwind/static/windows/js/when',
         'mvi': 'https://storage.googleapis.com/seoulwind/static/windows/mvi'
